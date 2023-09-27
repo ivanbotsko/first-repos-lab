7eec7c8f1627d30c5f93434cc9b5add90366668c
ivanbotsko <ivan.botsko.ik.2023@lpnu.ua>
2023-09-27 23:28:07 +0300

    first branch_02 commit

--- PR2.1/PR2.1.cpp
+++ PR2.1/PR2.1.cpp
@@ -1,26 +1,16 @@
 ﻿#include <iostream>
-#include <cmath>
-
 using namespace std;
-
 int main()
 {
-    double a; // вхідний параметр
-    double z1; // результат обчислення 1-го виразу
-    double z2; //  результат обчислення 2-го виразу
-
-    cout << "Enter the value of a: ";
-    cin >> a; // вставлено значення для 'a'
-
-    // Вирахування z1
-    z1 = (sin(a) + sin(5 * a) - sin(3 * a)) / (cos(a) - cos(3 * a) + cos(5 * a));
-
-    // Вираховування z2
-    z2 = tan(3 * a);
-
-    cout << "z1 = " << z1 << endl;
-    cout << "z2 = " << z2 << endl;
-
-    cin.get();
-    return 0;
+	double x; // вхідний параметр
+	// double z1; // результат обчислення 1-го виразу
+	double z2; // результат обчислення 2-го виразу
+	cout << "x = "; cin >> x;
+	// z1 = 2 * x;
+	z2 = x + x;
+	cout << endl;
+	// cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+	cin.get();
+	return 0;
 }
